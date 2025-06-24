d4bd261093d1cdcb06c9ad68bcf18a968d86d5c9
Anthony Aldea <tonythecoder@outlook.com>
2025-06-24 10:49:32 -0400

    correcting template literal

--- SampleFolder/calculate_Area.js
+++ SampleFolder/calculate_Area.js
@@ -6,9 +6,5 @@ function calculateAra() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
     let area = length * width;
-    document.getElementById('result').innerText = ` ${area}`;
-
-
-    
-    ` 
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
