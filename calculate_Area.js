73181a28ca2fd07577e18fde99703f2dc6b7cc3f
Anthony Aldea <tonythecoder@outlook.com>
2025-06-24 10:42:24 -0400

    first ibm lab push.

---
+++ calculate_Area.js
@@ -0,0 +1,14 @@
+let length;
+let width;
+
+
+function calculateAra() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+    let area = length * width;
+    document.getElementById('result').innerText = ` ${area}`;
+
+
+    
+    ` 
+}
